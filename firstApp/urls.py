1b58249e4936a97f012de8845966aa7f9b27ec59
Jaya Prakash Peddi <jayprakash1405401@gmail.com>
2025-02-11 21:47:31 +0530

    Delete firstApp directory

--- firstApp/urls.py
+++
@@ -1,6 +0,0 @@
-
-from django.urls import path
-from firstApp import views
-urlpatterns = [
-    path('wish/', views.wish),
-]
--- firstApp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def wish(Request):
-    return render(Request, 'firstApp/index.html')
