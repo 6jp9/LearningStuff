067e78ee7d56f719d2bf6f146b069749fb3367d7
6jp9 <jayprakash1405401@gmail.com>
2025-02-21 22:12:06 +0530

    Simple session practice work correction

--- SimpleDjangoSessionProject/testapp/views.py
+++ SimpleDjangoSessionProject/testapp/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import render
 from testapp.forms import TestForm
 # Create your views here.
 def display_form(request):
-    form = TestForm()
+    
     if request.method == 'POST':
         form = TestForm(request.POST)
         if form.is_valid():
-            request.session['name'] = form.cleaned_data['name']
-            request.session['quantity'] = form.cleaned_data['quantity']
-            print(request.COOKIES)
-            print(request.session.get_expiry_age())
+            name = form.cleaned_data['name']
+            quantity = form.cleaned_data['quantity']
+            request.session[name] = quantity
+            print('*'*60)
+            request.session.set_expiry(20)
+            print(request.session.get_expiry_date())
+    form = TestForm()
     return render(request,'testapp/index.html',{'form':form})
 
 def data(request):
-    name = request.session['name']
-    quantity = request.session['quantity']
-    return render(request,'testapp/table.html',{'name':name,'quantity':quantity})
+    return render(request,'testapp/table.html')
 
